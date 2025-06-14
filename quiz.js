b300e67bc28c12dc9bc5f45a3a165828e43c7e9d
PC <winniewaiguru@gmail.com>
2025-06-14 13:17:35 +0300

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("submit-answer")
+    .addEventListener("click", function checkAnswer() {
+      let correctAnswer = "4";
+      let userAnswer = document.querySelector(
+        'input[name="quiz"]:checked'
+      )?.value;
+      const feedback = document.getElementById("feedback");
+
+      if (userAnswer == correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+      } else {
+        feedback.textContent = "That's incorrect. Try again!";
+      }
+    });
+});
